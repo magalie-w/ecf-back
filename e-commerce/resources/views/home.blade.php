2bfc2ad5255d50ee88055c17ab1a9526261af318
magalie <magalie.waligora@gmail.com>
2022-09-27 13:35:25 +0200

    avancé front

--- e-commerce/resources/views/home.blade.php
+++ e-commerce/resources/views/home.blade.php
@@ -5,5 +5,173 @@
 @endsection
 
 @section('content')
-    <h1>Accueil</h1>
+    <div class="space-y-5">
+        <div class="text-center px-5 py-11 mx-[200px]">
+            <h1 class="text-4xl">E-Commerce</h1>
+
+            <p class="text-lg font-extralight text-zinc-800">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum delectus ad quae cumque voluptates dolorum, neque eveniet, placeat obcaecati magnam vel fugit nulla autem, mollitia consequuntur praesentium sit? Veniam, facere.
+            </p>
+        </div>
+
+        <div class="mx-[200px] flex justify-around">
+            <div class="w-[1000px] h-[400px] bg-blue-100">
+
+            </div>
+
+            <div class="w-[300px] h-[50px] bg-blue-100">
+                <div class="bg-green-500 p-2">Coup de coeur</div>
+
+                <div class="bg-zinc-600 h-[200px]"></div>
+
+                <div class="space-y-6">
+                    <p class="text-center">Produit</p>
+                    
+                    <p class="px-5">
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    </p>
+
+                    <div class="flex justify-center space-x-6">
+                        <button type="button" class="bg-green-500 py-2 px-5 rounded">99,00 €</button>
+
+                        <button type="button" class="bg-red-500 py-2 px-5 rounded">Voir</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{-- DERNIERS PRODUITS --}}
+        <div class="mx-[250px]">
+            <div>
+                <h2 class="bg-blue-500 text-white p-2 rounded-t">DERNIERS PRODUITS</h2>
+            </div>
+
+            <div class="flex justify-between">
+
+                <div class="w-[300px] space-y-5">
+                    <div class="w-[300px] h-[200px] bg-zinc-300">
+                    </div>
+
+                    <p class="text-blue-500 text-lg">Produit</p>
+
+                    <p class="text-center px-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+
+                    <div class="flex justify-center space-x-6">
+                        <button type="button" class="bg-green-500 py-2 px-5 rounded">99,00 €</button>
+                        <button type="button" class="bg-red-500 py-2 px-5 rounded">Ajouter</button>
+                    </div>
+                </div>
+
+                <div class="w-[300px] space-y-5">
+                    <div class="w-[300px] h-[200px] bg-zinc-300">
+                    </div>
+
+                    <p class="text-blue-500 text-lg">Produit</p>
+
+                    <p class="text-center px-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+
+                    <div class="flex justify-center space-x-6">
+                        <button type="button" class="bg-green-500 py-2 px-5 rounded">99,00 €</button>
+                        <button type="button" class="bg-red-500 py-2 px-5 rounded">Ajouter</button>
+                    </div>
+                </div>
+
+
+                <div class="w-[300px] space-y-5">
+                    <div class="w-[300px] h-[200px] bg-zinc-300">
+                    </div>
+
+                    <p class="text-blue-500 text-lg">Produit</p>
+
+                    <p class="text-center px-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+
+                    <div class="flex justify-center space-x-6">
+                        <button type="button" class="bg-green-500 py-2 px-5 rounded">99,00 €</button>
+                        <button type="button" class="bg-red-500 py-2 px-5 rounded">Ajouter</button>
+                    </div>
+                </div>
+
+                <div class="w-[300px] space-y-5">
+                    <div class="w-[300px] h-[200px] bg-zinc-300">
+                    </div>
+
+                    <p class="text-blue-500 text-lg">Produit</p>
+
+                    <p class="text-center px-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+
+                    <div class="flex justify-center space-x-6">
+                        <button type="button" class="bg-green-500 py-2 px-5 rounded">99,00 €</button>
+                        <button type="button" class="bg-red-500 py-2 px-5 rounded">Ajouter</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        {{-- MEILLEURS PRODUITS --}}
+        <div class="mx-[250px]">
+            <div>
+                <h2 class="bg-blue-500 text-white p-2 rounded-t">MEILLEURS PRODUITS</h2>
+            </div>
+
+            <div class="flex justify-between">
+
+                <div class="w-[300px] space-y-5">
+                    <div class="w-[300px] h-[200px] bg-zinc-300">
+                    </div>
+
+                    <p class="text-blue-500 text-lg">Produit</p>
+
+                    <p class="text-center px-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+
+                    <div class="flex justify-center space-x-6">
+                        <button type="button" class="bg-green-500 py-2 px-5 rounded">99,00 €</button>
+                        <button type="button" class="bg-red-500 py-2 px-5 rounded">Ajouter</button>
+                    </div>
+                </div>
+
+                <div class="w-[300px] space-y-5">
+                    <div class="w-[300px] h-[200px] bg-zinc-300">
+                    </div>
+
+                    <p class="text-blue-500 text-lg">Produit</p>
+
+                    <p class="text-center px-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+
+                    <div class="flex justify-center space-x-6">
+                        <button type="button" class="bg-green-500 py-2 px-5 rounded">99,00 €</button>
+                        <button type="button" class="bg-red-500 py-2 px-5 rounded">Ajouter</button>
+                    </div>
+                </div>
+
+
+                <div class="w-[300px] space-y-5">
+                    <div class="w-[300px] h-[200px] bg-zinc-300">
+                    </div>
+
+                    <p class="text-blue-500 text-lg">Produit</p>
+
+                    <p class="text-center px-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+
+                    <div class="flex justify-center space-x-6">
+                        <button type="button" class="bg-green-500 py-2 px-5 rounded">99,00 €</button>
+                        <button type="button" class="bg-red-500 py-2 px-5 rounded">Ajouter</button>
+                    </div>
+                </div>
+
+                <div class="w-[300px] space-y-5">
+                    <div class="w-[300px] h-[200px] bg-zinc-300">
+                    </div>
+
+                    <p class="text-blue-500 text-lg">Produit</p>
+
+                    <p class="text-center px-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+
+                    <div class="flex justify-center space-x-6">
+                        <button type="button" class="bg-green-500 py-2 px-5 rounded">99,00 €</button>
+                        <button type="button" class="bg-red-500 py-2 px-5 rounded">Ajouter</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- e-commerce/resources/views/layouts/base.blade.php
+++ e-commerce/resources/views/layouts/base.blade.php
@@ -11,8 +11,8 @@
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 <body>
-    <header>
-        <div class="bg-zinc-800 text-white flex justify-around">
+    <header class="mx-auto w-full bg-zinc-800">
+        <div class="text-white flex justify-around p-5 items-center">
             <div>
                 <ul class="flex space-x-5">
                     <li>
@@ -32,7 +32,24 @@
                 </ul>
             </div>
 
-            <div>
+            {{-- RECHERCHE --}}
+            <div class="flex items-center space-x-5">
+                <form>
+                    <div class="flex">
+                        <input type="text" placeholder="Recherche.." class="rounded-l pl-1">
+                        <button class="bg-zinc-500 p-1 rounded-r" type="button">
+                            <img src="img/search.svg" alt="" width="25">
+                        </button>
+                    </div>
+                </form>
+
+                {{-- PANIER --}}
+                <div class="bg-green-500 p-2 rounded">
+                    <p>Panier</p>
+                </div>
+
+
+                {{-- CONNEXION / INSCRIPTION --}}
                 <ul class="flex space-x-5">
                     <li>
                         <a href="{{ route('connexion') }}">Connexion</a>
@@ -48,41 +65,60 @@
     
     @yield('content')
 
-    <footer>
-        <div>
-            <div>
-                <li>A propos</li>
+    <footer class="bg-zinc-800 text-white p-5">
+        <div class="mx-auto container flex justify-around">
+            <div class="w-[300px] space-y-5">
+                <h5>A propos</h5>
+
+                <hr>
+
                 <p>
                     Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression.
                 </p>
             </div>
 
-            <div>
-                <li>Informations</li>
+            <div class="space-y-5">
+                <h5>Informations</h5>
 
-                <li>Link 1</li>
-                <li>Link 2</li>
-                <li>Link 3</li>
-                <li>Link 4</li>
+                <hr>
+
+                <ul class="list-none">
+                    <li>Link 1</li>
+                    <li>Link 2</li>
+                    <li>Link 3</li>
+                    <li>Link 4</li>
+                </ul>
             </div>
 
-            <div>
-                <li>Others Links</li>
+            <div class="space-y-5">
+                <h5>Others Links</h5>
 
-                <li>Link 1</li>
-                <li>Link 2</li>
-                <li>Link 3</li>
-                <li>Link 4</li>
+                <hr>
+
+                <ul class="list-none">
+                    <li>Link 1</li>
+                    <li>Link 2</li>
+                    <li>Link 3</li>
+                    <li>Link 4</li>
+                </ul>
             </div>
 
-            <div>
-                <li>Contact</li>
+            <div class="space-y-5">
+                <h5>Contact</h5>
 
-                <li>My Company</li>
-                <li>email@example.com</li>
-                <li>03 12 23 45 56</li>
-                <li>06 56 56 48 58</li>
+                <hr>
+
+                <ul class="list-none">
+                    <li>My Company</li>
+                    <li>email@example.com</li>
+                    <li>03 12 23 45 56</li>
+                    <li>06 56 56 48 58</li>
+                </ul>
             </div>
+        </div>
+
+        <div class="mx-auto container">
+            <a href="#">Back to top</a>
         </div>
     </footer>
 </body>
